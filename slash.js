0e14f68ea96adddb61baa200789f95120a5f6c8a
Joseph <114080998+Yu9f@users.noreply.github.com>
2023-10-21 05:18:28 +0300

    Update slash.js

--- slash.js
+++ slash.js
@@ -1,9 +1,8 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord.js');
+const { Routes, REST } = require('discord.js');
 const { readdirSync } = require('fs');
+const config = require("./config.json");
 const path = require('path');
 require('colors');
-require('dotenv').config();
 
 // setup slash commands
 
@@ -13,14 +12,14 @@ readdirSync("./slash/").map(async dir => {
 	commands.push(require(path.join(__dirname, `./slash/${dir}/${cmd}`)))
     })
 })
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(config.TOKEN);
 
 (async () => {
 	try {
 		console.log('[Discord API] Started refreshing application (/) commands.'.yellow);
 		await rest.put(
 			// if you want to make your slash commands in all guilds use "applicationCommands("CLIENT_ID")"
-			Routes.applicationGuildCommands(process.env.BOTID, process.env.SERVERID),
+			Routes.applicationGuildCommands(config.CLIENTID, config.GUILID),
 			{ body: commands },
 		);
 		console.log('[Discord API] Successfully reloaded application (/) commands.'.green);
