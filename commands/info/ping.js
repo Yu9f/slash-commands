15f8d829c3f2d80c65dde191175bc93fbff06530
Joseph <114080998+Yu9f@users.noreply.github.com>
2024-03-16 02:15:13 +0300

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,14 +1,14 @@
-const Discord = require('discord.js');
+const { EmbedBuilder, } = require("discord.js");
 
 module.exports = {
-	name: 'ping',
-	description: 'Get bot speed',
+	name: "ping",
+	description: "Get bot speed",
 	timeout: 5000,
 	run: async (client, message) => {
-		const msg = await message.channel.send('Pong!');
-		const embed = new Discord.MessageEmbed()
+		const msg = await message.channel.send("Pong!");
+		const embed = new EmbedBuilder()
 			.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-			.setColor('RANDOM')
+			.setColor("GREEN")
 			.setTimestamp()
 			.setDescription(
 				`**Time:** ${Math.floor(msg.createdTimestamp - message.createdTimestamp)} ms\n**API Ping:** ${
